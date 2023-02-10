9028f3e3b4b8d4ee465f758851449e092657d083
Grégoire Marabout-Demazure <gmarabout@gmail.com>
2023-02-10 11:29:30 +0100

    Moved main file to root folder, add a sample pic and better defaults for face recognition

--- main.py
+++ main.py
@@ -5,25 +5,25 @@ face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 number_of_faces_detected = 0
 selected_file = None
-img = None
+image = None
 
 
 def process_image(state):
     img = cv2.imread(state.selected_file, cv2.IMREAD_UNCHANGED)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 3)
     state.number_of_faces_detected = len(faces)
     # Draw a rectangle around faces
     for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 0), 5)
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 0), 2)
 
-    state.img = cv2.imencode(".jpg", img)[1].tobytes()
+    state.image = cv2.imencode(".jpg", img)[1].tobytes()
 
 
 content = """
 <|{selected_file}|file_selector|label=Upload File|on_action=process_image|extensions=.jpg,.gif,.png|drop_message=Drop Message|>
 
-<|{img}|image|width=300px|height=300px|>
+<|{image}|image|width=300px|height=300px|>
 
 <|{number_of_faces_detected} face(s) detected|>
 """
